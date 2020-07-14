5151ebfb3c848194977833e53c60db37a90d45f4
v2verma1990 <vishal.is.verma@gmail.com>
2020-07-14 21:47:51 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
 import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,7 +18,8 @@ import { CartService } from './cart.service';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path: 'products/:productId',component: ProductDetailsComponent }
+      {path: 'products/:productId',component: ProductDetailsComponent },{ path: 'cart', component: CartComponent }
+      
     ])
   ],
   declarations: [
@@ -26,6 +28,7 @@ import { CartService } from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
       ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
